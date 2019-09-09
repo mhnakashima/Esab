041be607b641b69c29a647e309e150532b88690c
michael haruo nakashima <mhnakashima@gmail.com>
2019-09-09 01:26:14 -0300

    Items - produção 09/09

---
+++ telas/dev/sld37_old/tela.js
@@ -0,0 +1,26 @@
+//VARIAVEIS GLOBAIS ---------------------------------------------------------------
+
+// DEFINE O TEMPO PADRAO DE CARREGAMENTO DAS ANIMACOES EM OBJETOS
+var tempo_entrada = 500;
+var num_obj = 9
+
+//FUNÇÕES -------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+//FUNCAO INICIA EXIBIÇÃO DE OBJETOS DA CAPA  --------------------------------------
+
+function iniciaScript_fc()
+{		
+	//TOCA AUDIO DA CAPA
+ 	//toca_audio('audioCapa');
+	
+	//CRIA EFEITOS DE EXIBIÇÃO NA PÁGINA
+	parent.habilitaAvancar();
+	
+}
+
+
+
+
+
+
+
